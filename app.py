fdbf1c25ac87a5e197e1496cbca57a4ffa079770
VergeDX <neko@hydev.org>
2020-09-13 22:27:36 +0800

    [+] code comment

--- app.py
+++ app.py
@@ -10,21 +10,25 @@ def rss_filter():
     rss_url = request.args.get('rss_url')
     title_contains = request.args.get('title_contains')
 
+    # Need 2 args, or else show usage.
     if not (rss_url and title_contains):
         return 'Need rss_url & title_contains as query string, \n' \
                'this will return a new rss with title filter. \n' \
                '(Apply $title_contains for each item from $rss_url). '
 
     base_rss = feedparser.parse(rss_url)
+
     # No base_rss.feed, seems cannot fetch origin rss url.
     if not base_rss.feed:
         return 'Cannot fetch rss url\'s feed title, \n' \
                'please check the url: \n' + rss_url
 
+    # Filtered rss entries.
     result_rss_entries = []
     for rss_entry in base_rss.entries:
         if title_contains in rss_entry.title:
             result_rss_entries.append(rss_entry)
+
     return build_rss_string(result_rss_entries, base_rss.feed.title, title_contains)
 
 
