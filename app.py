523153a4ff5ea7f2a8ad6e9e5fe327244a238ad4
VergeDX <neko@hydev.org>
2020-09-15 16:17:28 +0800

    [+] using lxml to parse & remove

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import feedparser
-from feedgen.feed import FeedGenerator
+import lxml
+import requests
 from flask import Flask, Response, request
+from lxml import etree
 
 app = Flask(__name__)
 
@@ -16,39 +17,25 @@ def rss_filter():
                'this will return a new rss with title filter. \n' \
                '(Apply $title_contains for each item from $rss_url). '
 
-    base_rss = feedparser.parse(rss_url)
+    try:
+        base_xml = requests.get(rss_url).content
+    except requests.exceptions.MissingSchema as e:
+        return Response(e.__str__())
 
-    # No base_rss.feed, seems cannot fetch origin rss url.
-    if not base_rss.feed:
-        return 'Cannot fetch rss url\'s feed title, \n' \
-               'please check the url: \n' + rss_url
+    try:
+        root = etree.fromstring(base_xml)
+    except lxml.etree.XMLSyntaxError as e:
+        return Response('Link may not xml: ' + e.__str__())
 
-    # Filtered rss entries.
-    result_rss_entries = []
-    for rss_entry in base_rss.entries:
-        if title_contains in rss_entry.title:
-            result_rss_entries.append(rss_entry)
+    if root[0].tag != 'channel':
+        return Response('Link %s may not rss. ' % (rss_url,))
 
-    return build_rss_string(result_rss_entries, base_rss.feed.title, title_contains)
+    for child in root[0]:
+        # TODO: child[0] should be title...?
+        if child.tag == 'item' and title_contains not in child[0].text:
+            root[0].remove(child)
 
-
-def build_rss_string(result_rss_entries, base_rss_title, title_contains):
-    # Create a feed, https://github.com/lkiesow/python-feedgen#create-a-feed
-    fg = FeedGenerator()
-    fg.title('[%s] filtered [%s]' % (title_contains, base_rss_title))
-    fg.link(href='https://github.com/VergeDX/rss_filter_server')
-    fg.description('Rss filter, writen by HyDEV : )')
-
-    for rss_entry in result_rss_entries:
-        # Add feed entries.
-        # https://github.com/lkiesow/python-feedgen#add-feed-entries
-        fe = fg.add_entry()
-        fe.title(rss_entry.title)
-        fe.link(href=rss_entry.link)
-
-    # Using response with mine type xml, avoid the browser parse.
-    # https://stackoverflow.com/questions/11773348/python-flask-how-to-set-content-type
-    return Response(fg.rss_str(pretty=True), mimetype='text/xml')
+    return Response(etree.tostring(root), mimetype='text/xml')
 
 
 if __name__ == '__main__':
