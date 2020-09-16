027fd5eae6ffc35c8b94c70177effa6795dee355
VergeDX <neko@hydev.org>
2020-09-16 13:03:09 +0800

    [+] /github_releases node, merge lastest release info

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import requests
+from feedgen.feed import FeedGenerator
 from flask import Flask, Response, request
 from lxml import etree
 
-# TODO: rss item merge.
 app = Flask(__name__)
+
+# https://developer.github.com/v3/repos/releases/#get-the-latest-release
+REPOS_API = 'https://api.github.com/repos/'
+LATEST = '/releases/latest'
 
 
 @app.route('/')
@@ -35,6 +39,34 @@ def rss_filter():
 
     except Exception as e:
         return Response('Error: ' + e.__str__())
+
+
+@app.route('/github_releases')
+def github_releases():
+    repos_arg = request.args.get('repos')
+    if not repos_arg:
+        return Response('Need repos arg. ')
+
+    repo_list = repos_arg.split(', ')
+
+    # https://github.com/lkiesow/python-feedgen#create-a-feed
+    fg = FeedGenerator()
+
+    # ValueError: Required fields not set (title, link, description)
+    fg.title('GitHub multi-repo releases tracker')
+    fg.link(href='https://github.com/VergeDX/rss_filter_server')
+    fg.description('Tracker more repo\'s release in one rss link! '
+                   'Written by HyDEV, thanks for using. ')
+
+    for repo_str in repo_list:
+        r_json = requests.get(REPOS_API + repo_str + LATEST).json()
+        if 'message' not in r_json:
+            # https://github.com/lkiesow/python-feedgen#add-feed-entries
+            fe = fg.add_item()
+            fe.title('[%s] %s' % (repo_str, r_json['name']))
+            fe.link(href=r_json['html_url'])
+
+    return Response(fg.rss_str(), mimetype='text/xml')
 
 
 if __name__ == '__main__':
